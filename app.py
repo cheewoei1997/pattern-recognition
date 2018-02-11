627dbf28636027c158f41c9a3ceedab6c5f5b0d3
cheewoei1997 <cheewoei1997@gmail.com>
2018-02-12 04:34:27 +0800

    basic web

--- app.py
+++ app.py
@@ -1,29 +1,49 @@
-from flask import Flask, render_template, flash, redirect
-from flask_wtf import FlaskForm
-from database import db_session
-from models import User, Post
-from config import Config
-from models import LoginForm
+import os
+from flask import Flask, render_template, flash, redirect, request, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 
+UPLOAD_FOLDER = '/home/cheewoei/Documents/test'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
-def upload():
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-    all_users = User.query.all()
-    all_posts = Post.query.all()
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
-    context = {
-    'all_users': all_users,
-    'all_posts': all_posts,
-    }
-    return render_template('landing.html', context = context)
-
-@app.teardown_appcontext
-def shutdown_session(exception=None):
-    db_session.remove()
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)    
 
 if __name__ == '__main__':
     app.run()
