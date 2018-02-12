1a8e5a7aac06d79569f1ecaa6ab1df90090e8dae
cheewoei1997 <cheewoei1997@gmail.com>
2018-02-12 12:28:01 +0800

    classifier

---
+++ NeuralN.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+x = np.loadtxt('TrainAll.txt')
+y = np.loadtxt('TrainLabels.txt')
--- TrainLabels.py
+++ TrainLabels.py
@@ -5,4 +5,10 @@ labels =   [0,1,2,1,0,2,1,1,2,1,0,2,0,2,1,1,1,0,1,0,0,0,0,1,
             0,0,1,2,2,2,2,2,2,2,2,2,1,1,0,0,0,0,0,0,0,0,0,0,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1]
 
 labels = np.array(labels)
-#print(labels)
+
+# data = np.loadtxt('TrainAll.txt')
+
+# print(labels)
+# print(labels.shape)
+# print(data)
+# print(data.shape)
