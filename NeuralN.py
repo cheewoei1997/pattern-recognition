2e13336ed21ecaa5eccd76fa6e0c6ba7a59503a3
cheewoei1997 <cheewoei1997@gmail.com>
2018-02-12 13:13:41 +0800

    neural network

--- NeuralN.py
+++ NeuralN.py
@@ -1,4 +1,25 @@
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
 x = np.loadtxt('TrainAll.txt')
 y = np.loadtxt('TrainLabels.txt')
+
+x_train, x_test, y_train, y_test = train_test_split(x , y)
+print(x.shape)
+print(y.shape)
+
+scaler = StandardScaler()
+scaler.fit(x_train)
+
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+
+mlp = MLPClassifier(hidden_layer_sizes = (93, 93, 93))
+mlp.fit(x_train, y_train)
+predictions = mlp.predict(x_test)
+
+print(confusion_matrix(y_test, predictions))
+print(accuracy_score(y_test, predictions))
--- TrainLabels.py
+++ TrainLabels.py
@@ -5,10 +5,12 @@ labels =   [0,1,2,1,0,2,1,1,2,1,0,2,0,2,1,1,1,0,1,0,0,0,0,1,
             0,0,1,2,2,2,2,2,2,2,2,2,1,1,0,0,0,0,0,0,0,0,0,0,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1]
 
 labels = np.array(labels)
+np.savetxt('TrainLabels.txt', labels)
+
 
 # data = np.loadtxt('TrainAll.txt')
 
-# print(labels)
-# print(labels.shape)
+print(labels)
+print(labels.shape)
 # print(data)
 # print(data.shape)
